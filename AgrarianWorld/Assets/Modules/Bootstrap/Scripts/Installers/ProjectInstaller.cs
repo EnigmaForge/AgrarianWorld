93cabeffb90912538bb4e0c9cb3970a2bb6d1c0c
Yevhen <yerohozhyn@gmail.com>
2024-02-25 14:59:17 +0200

    Added timers manager to project context

--- AgrarianWorld/Assets/Modules/Bootstrap/Scripts/Installers/ProjectInstaller.cs
+++ AgrarianWorld/Assets/Modules/Bootstrap/Scripts/Installers/ProjectInstaller.cs
@@ -1,6 +1,7 @@
 using GameUpdatersModule;
 using SceneLoaderModule;
 using StateMachine;
+using TimerModule;
 using UnityEngine;
 using Zenject;
 
@@ -8,19 +9,40 @@ namespace Bootstrap.Installers {
     [CreateAssetMenu(fileName = nameof(ProjectInstaller), menuName = "Enigma Forge/Global Installers/" + nameof(ProjectInstaller))]
     public class ProjectInstaller : ScriptableObjectInstaller {
         public override void InstallBindings() {
+            BindUpdaters();
+            BindSceneLoader();
+            BindStateMachines();
+            BindTimerSystem();
+        }
+
+        private void BindUpdaters() {
             Container.Bind<IGameUpdater>()
                      .To<GameUpdater>()
                      .FromNewComponentOnNewGameObject()
                      .AsSingle()
                      .NonLazy();
+        }
 
+        private void BindSceneLoader() {
             Container.BindInterfacesAndSelfTo<SceneLoader>()
                      .AsSingle()
                      .NonLazy();
+        }
 
+        private void BindStateMachines() {
             Container.BindInterfacesAndSelfTo<GameStateMachine>()
                      .AsSingle()
                      .NonLazy();
+        }
+
+        private void BindTimerSystem() {
+            Container.Bind<ITimersFactory>()
+                     .To<TimersFactory>()
+                     .AsSingle();
+
+            Container.Bind<ITimersManager>()
+                     .To<TimersManager>()
+                     .AsSingle();
         }
     }
 }
