91dbdc1a9c6e1b62ff2f620c1ab8d91b4254b258
Yevhen <yerohozhyn@gmail.com>
2024-04-28 16:25:00 +0300

    🔥 remove debug module and draft menu

--- AgrarianWorld/Assets/Modules/Core/Bootstraps/Scripts/InitialSceneBootstrap.cs
+++ AgrarianWorld/Assets/Modules/Core/Bootstraps/Scripts/InitialSceneBootstrap.cs
@@ -4,19 +4,13 @@ using Zenject;
 
 namespace Modules.Core.Bootstraps {
     public class InitialSceneBootstrap : MonoBehaviour {
-        [SerializeField] private GameObject _debugMenuPrefab;
-        
         private void Start() {
             InitializeContext();
-            CreateDebugMenu();
             LoadGameMenu();
         }
 
         private void InitializeContext() =>
             ProjectContext.Instance.EnsureIsInitialized();
-
-        private void CreateDebugMenu() =>
-            ProjectContext.Instance.Container.InstantiatePrefab(_debugMenuPrefab);
 
         private void LoadGameMenu() {
             IGameStateMachine gameStateMachine = ProjectContext.Instance.Container.Resolve<IGameStateMachine>();
